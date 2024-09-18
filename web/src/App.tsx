f609cec6c1a60c7ecec6a51357d08a25ee4f983b
Danrlei <danrleidalfre@gmail.com>
2024-09-17 23:08:27 -0300

    change to theme light and dark

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,10 @@
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
+
 export function App() {
-  return <h1>Olá</h1>
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
+    </ThemeProvider>
+  )
 }
