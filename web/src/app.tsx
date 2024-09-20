25808d2de497b57762fb711c31951fb43fd2da90
Danrlei <danrleidalfre@gmail.com>
2024-09-19 22:23:28 -0300

    web: change eslint to biome

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,5 +1,5 @@
-import { BicepsFlexed } from "lucide-react";
-import { Button } from "./components/ui/button";
+import { Button } from '@/components/ui/button'
+import { BicepsFlexed } from 'lucide-react'
 
 export function App() {
   return (
