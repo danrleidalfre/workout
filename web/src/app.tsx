aa07cf37c9f35272a01be0a68d5a28688120c190
Danrlei <danrleidalfre@gmail.com>
2024-09-20 00:24:54 -0300

    web: add toggle theme

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,6 +1,11 @@
+import { ThemeProvider } from '@/pages/_layout/header/toggle-theme/provider'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  )
 }
--- web/src/pages/_layout/header/index.tsx
+++ web/src/pages/_layout/header/index.tsx
@@ -2,12 +2,13 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Separator } from '@/components/ui/separator'
 import { BicepsFlexed } from 'lucide-react'
 import { NavLink } from './nav-link'
+import { ToggleTheme } from './toggle-theme'
 
 export function Header() {
   return (
     <header className="flex justify-between items-center h-20 px-8 border-b">
       <div className="flex items-center gap-4">
-        <BicepsFlexed className="size-10 text-primary" />
+        <BicepsFlexed className="size-10 text-primary" strokeWidth={1} />
 
         <Separator orientation="vertical" className="h-8" />
 
@@ -17,10 +18,14 @@ export function Header() {
           <NavLink to="/exercises">Exercises</NavLink>
         </nav>
       </div>
-      <Avatar>
-        <AvatarImage src="https://github.com/danrleidalfre.png" />
-        <AvatarFallback>DF</AvatarFallback>
-      </Avatar>
+
+      <div className="flex gap-4">
+        <ToggleTheme />
+        <Avatar>
+          <AvatarImage src="https://github.com/danrleidalfre.png" />
+          <AvatarFallback>DF</AvatarFallback>
+        </Avatar>
+      </div>
     </header>
   )
 }
