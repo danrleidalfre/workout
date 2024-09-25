a6b29667ea7d80bdf3b45aa435f1095d2ea56314
Danrlei <danrleidalfre@gmail.com>
2024-09-25 01:04:01 -0300

    web: add list and actions exercise and sheet create

--- web/src/pages/exercises/index.tsx
+++ web/src/pages/exercises/index.tsx
@@ -1,12 +1,8 @@
 import { fetchExercises } from '@/api/fetch-exercises'
 import { fetchGroups } from '@/api/fetch-groups'
+import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import {
   Select,
@@ -15,8 +11,18 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 import { useQuery } from '@tanstack/react-query'
-import { PlusCircle } from 'lucide-react'
+import { Edit3, PlusCircle, Trash2 } from 'lucide-react'
 
 export function Exercises() {
   const exercises = useQuery({
@@ -49,17 +55,56 @@ export function Exercises() {
             </SelectContent>
           </Select>
         </div>
-        <Button className="gap-1">
-          <PlusCircle className="size-4" />
-          <span>Exercício</span>
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className="gap-1">
+              <PlusCircle className="size-4" />
+              <span>Exercício</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Novo exercício</SheetTitle>
+            </SheetHeader>
+            <SheetDescription className="grid gap-4 my-4">
+              <Input placeholder="Nome do exercício" />
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione o grupo muscular" />
+                </SelectTrigger>
+                <SelectContent>
+                  {groups.data?.map(group => (
+                    <SelectItem key={group.id} value={group.id}>
+                      {group.group}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </SheetDescription>
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button type="submit">Salvar</Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
       </div>
       <div className="grid grid-cols-4 gap-4">
         {exercises.data?.map(exercise => (
           <Card key={exercise.id}>
             <CardHeader>
-              <CardTitle>{exercise.exercise}</CardTitle>
-              <CardDescription>{exercise.group}</CardDescription>
+              <CardTitle className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <h2 className="mr-2">{exercise.exercise}</h2>
+                  <Button size="icon" variant="link">
+                    <Edit3 className="size-4" />
+                  </Button>
+                  <Button size="icon" variant="link">
+                    <Trash2 className="size-4" />
+                  </Button>
+                </div>
+                <Badge>{exercise.group}</Badge>
+              </CardTitle>
             </CardHeader>
           </Card>
         ))}
