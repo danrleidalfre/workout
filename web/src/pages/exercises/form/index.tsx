0978a58824e2263466307fa0be63f711523180d4
Danrlei <danrleidalfre@gmail.com>
2024-09-28 01:07:30 -0300

    web: create exercise and load button

--- web/src/pages/exercises/form/index.tsx
+++ web/src/pages/exercises/form/index.tsx
@@ -1,19 +1,47 @@
+import { createExercise } from '@/api/create-exercise'
 import { SelectGroup } from '@/components/select-group'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import {
   Sheet,
-  SheetClose,
   SheetContent,
-  SheetDescription,
   SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet'
-import { PlusCircle } from 'lucide-react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { CheckCircle2, LoaderCircle, PlusCircle } from 'lucide-react'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  title: z.string(),
+  groupId: z.string(),
+})
+
+type FormSchema = z.infer<typeof formSchema>
 
 export function ExerciseForm() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { isSubmitting },
+  } = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+  })
+
+  const handleSave = async ({ title, groupId }: FormSchema) => {
+    await createExercise({ title, groupId })
+
+    reset({
+      title: '',
+      groupId: '',
+    })
+  }
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -26,16 +54,45 @@ export function ExerciseForm() {
         <SheetHeader>
           <SheetTitle>Novo exercício</SheetTitle>
         </SheetHeader>
-        <SheetDescription>
-          <form className="grid gap-4 my-4">
-            <Input placeholder="Nome do exercício" />
-            <SelectGroup />
-          </form>
-        </SheetDescription>
+        <form
+          onSubmit={handleSubmit(handleSave)}
+          id="form"
+          className="grid gap-4 my-4"
+        >
+          <Input
+            placeholder="Nome do exercício"
+            required
+            {...register('title')}
+          />
+          <Controller
+            name="groupId"
+            control={control}
+            render={({ field: { name, onChange, value } }) => {
+              return (
+                <SelectGroup
+                  name={name}
+                  onValueChange={onChange}
+                  value={value}
+                  required
+                />
+              )
+            }}
+          />
+        </form>
         <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit">Salvar</Button>
-          </SheetClose>
+          <Button type="submit" form="form" className="items-center gap-1">
+            {isSubmitting ? (
+              <>
+                <LoaderCircle className="size-4 animate-spin" />
+                <span>Salvando...</span>
+              </>
+            ) : (
+              <>
+                <CheckCircle2 className="size-4" />
+                <span>Salvar</span>
+              </>
+            )}
+          </Button>
         </SheetFooter>
       </SheetContent>
     </Sheet>
