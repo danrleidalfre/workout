f3528e539da75065656e1dda0ddc25759145d8df
Danrlei <danrleidalfre@gmail.com>
2024-10-19 23:40:59 -0300

    web: skeleton graphs

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,16 +1,36 @@
+import { fetchDurationByWeekCompletions } from '@/api/fetch-duration-by-week-completions'
+import { fetchSeriesByGroupCompletions } from '@/api/fetch-series-by-group-completions'
+import { fetchVolumeByWeekCompletions } from '@/api/fetch-volume-by-week-completions'
+import { Skeleton } from '@/components/ui/skeleton'
+import { useQuery } from '@tanstack/react-query'
 import { HomeCard } from './card'
 import { HomeChartArea } from './charts/area'
 import { HomeChartBar } from './charts/bar'
 import { HomeChartRadar } from './charts/radar'
 
 export function Home() {
+  const { isFetching: isLoadingRadar } = useQuery({
+    queryKey: ['workouts-series-by-group-completions'],
+    queryFn: fetchSeriesByGroupCompletions,
+  })
+
+  const { isFetching: isLoadingArea } = useQuery({
+    queryKey: ['workouts-duration-by-week-completions'],
+    queryFn: fetchDurationByWeekCompletions,
+  })
+
+  const { isFetching: isLoadingBar } = useQuery({
+    queryKey: ['workouts-volume-by-week-completions'],
+    queryFn: fetchVolumeByWeekCompletions,
+  })
+
   return (
     <div className="grid grid-cols-[2fr_4fr_3fr] gap-4">
       <HomeCard />
-      <HomeChartRadar />
+      {isLoadingRadar ? <Skeleton className="size-full" /> : <HomeChartRadar />}
       <div className="grid gap-4">
-        <HomeChartBar />
-        <HomeChartArea />
+        {isLoadingBar ? <Skeleton className="size-full" /> : <HomeChartBar />}
+        {isLoadingArea ? <Skeleton className="size-full" /> : <HomeChartArea />}
       </div>
     </div>
   )
