1b8e4b3826f90fa17da3fe94c62b6e6fc2f3c8a2
Danrlei <danrleidalfre@gmail.com>
2024-11-03 18:00:49 -0300

    mobile: add title to toggle theme in profile screen

--- mobile/src/screens/profile/index.tsx
+++ mobile/src/screens/profile/index.tsx
@@ -1,10 +1,13 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { ToggleTheme } from "./toggle-theme";
 
 export function Profile() {
   return (
-    <View className="items-center mt-4">
-      <ToggleTheme />
+    <View className="items-center mt-4 px-6">
+      <View className="flex-row justify-between items-center w-full">
+        <Text className="text-muted-foreground dark:text-muted">Tema do app</Text>
+        <ToggleTheme />
+      </View>
     </View>
-  ) 
+  )
 }
--- mobile/src/screens/profile/toggle-theme/index.tsx
+++ mobile/src/screens/profile/toggle-theme/index.tsx
@@ -9,7 +9,7 @@ export function ToggleTheme() {
   return (
     <View className="flex-row">
       <TouchableOpacity className={cn(
-        'h-10 px-4 rounded-tl-full rounded-bl-full flex-row items-center justify-center gap-1',
+        'h-10 px-4 rounded-tl-md rounded-bl-md flex-row items-center justify-center gap-1',
         'bg-secondary dark:bg-secondary-foreground',
         { 'bg-primary': theme === 'light' }
       )} onPress={() => changeTheme('light')}>
@@ -28,7 +28,7 @@ export function ToggleTheme() {
         <Text className="text-muted-foreground dark:text-muted text-base font-medium">Dark</Text>
       </TouchableOpacity>
       <TouchableOpacity className={cn(
-        'h-10 px-4 rounded-tr-full rounded-br-full flex-row items-center justify-center gap-1',
+        'h-10 px-4 rounded-tr-md rounded-br-md flex-row items-center justify-center gap-1',
         'bg-secondary dark:bg-secondary-foreground',
         { 'bg-primary dark:bg-primary': theme === 'system' }
       )} onPress={() => changeTheme('system')}>
