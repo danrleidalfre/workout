8ec53176451610b135d6b7fc2bb920a2b230d203
Danrlei <danrleidalfre@gmail.com>
2024-11-05 00:39:12 -0300

    mobile: refactor routes

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,11 +1,8 @@
-import { AppLayout } from "@/layouts/app";
-import { AuthLayout } from "@/layouts/auth";
-import { Profile } from "@/screens/profile";
-import { Workout } from "@/screens/workout";
-import { Workouts } from "@/screens/workouts";
 import { useAuth } from "@clerk/clerk-expo";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator, NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { AppRoutes } from "./app";
+import { AuthRoutes } from "./auth";
 
 export type RoutesProps = {
   login: undefined;
@@ -15,44 +12,6 @@ export type RoutesProps = {
 };
 
 export type AppNavigatorRoutesProps = NativeStackNavigationProp<RoutesProps>;
-
-const { Screen, Navigator } = createNativeStackNavigator<RoutesProps>();
-
-function AppRoutes() {
-  return (
-    <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="workouts">
-        {() => (
-          <AppLayout>
-            <Workouts />
-          </AppLayout>
-        )}
-      </Screen>
-      <Screen name="workout">
-        {() => (
-          <AppLayout>
-            <Workout />
-          </AppLayout>
-        )}
-      </Screen>
-      <Screen name="profile">
-        {() => (
-          <AppLayout>
-            <Profile />
-          </AppLayout>
-        )}
-      </Screen>
-    </Navigator>
-  );
-}
-
-function AuthRoutes() {
-  return (
-    <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="login" component={AuthLayout} />
-    </Navigator>
-  );
-}
 
 export function Routes() {
   const { isSignedIn } = useAuth();
